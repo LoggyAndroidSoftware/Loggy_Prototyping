bf29a1f107cc359ddc30039db5799e182240b982
maxpp2910 <107290604+maxpp2910@users.noreply.github.com>
2024-06-14 00:19:49 -0500

    Create welcomeView.dart

--- Loggy_Prototyping_Project/lib/loginView.dart
+++ Loggy_Prototyping_Project/lib/loginView.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:loggy_prototyping_project/welcomeView.dart';
 class loginView extends StatelessWidget {
   const loginView({super.key});
 
---
+++ Loggy_Prototyping_Project/lib/welcomeView.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:loggy_prototype/production.dart';
+
+class welcomeView extends StatelessWidget {
+  const welcomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 2), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) {
+              return const production();
+            },
+          ),
+        );
+      });
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/logo.png',
+              height: 150,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Bienvenido, Usuario',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
